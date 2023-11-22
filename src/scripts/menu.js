22320f967c6f00ea8b8f445ccc3d9461596af8a1
justin <justinhuang2020@gmail.com>
2023-11-23 12:22:36 +1300

    Fix alignment issues

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,6 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
+    document.querySelector('.nav').classList.toggle('expanded-nav');
     document.querySelector('.logo').classList.toggle('hidden');
     document.querySelector('.hamburger').classList.toggle('expanded');
   });
