0afe87d72c8c2bbaac8280421762e3abecd0d016
justin <justinhuang2020@gmail.com>
2023-11-23 12:08:41 +1300

    Implement hamburger animation

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,5 +1,5 @@
 document.querySelector('.hamburger').addEventListener('click', () => {
     document.querySelector('.nav-links').classList.toggle('expanded');
     document.querySelector('.logo').classList.toggle('hidden');
-    document.querySelector('.hamburger').classList.toggle('hidden');
+    document.querySelector('.hamburger').classList.toggle('expanded');
   });
