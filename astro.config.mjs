993fd36b31563dcf78ab92773b5095b4cbb0f2a7
justin <jhua515@aucklanduni.ac.nz>
2023-12-03 14:02:17 +1300

    Integrate tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://justinfyh.github.io',
+  site: 'https://justinfyh.github.io',
+  integrations: [tailwind()]
 });
