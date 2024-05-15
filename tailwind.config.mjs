b2ccdcad8e66dfc88bafb2fe02d61e50af2ef553
justin <jhua515@aucklanduni.ac.nz>
2024-05-16 01:08:00 +1200

    Implement new website design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            colors: {
+                background: "#121212",
+                main: "#212121",
+                accent: "#1db954",
+                secondary: "#535353",
+            },
+        },
+    },
+    plugins: [],
+};
