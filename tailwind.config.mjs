1967b23d3c3094eba8de03716ae2acf6cb70f548
justin <justinhuang2020@gmail.com>
2024-08-17 01:04:43 +1200

    Install and enable prettier for astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-    theme: {
-        extend: {
-            colors: {
-                background: "#344E41",
-                main: "#3A5A40",
-                accent: "#DAD7CD",
-                secondary: "#588157",
-            },
-        },
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        background: "#344E41",
+        main: "#3A5A40",
+        accent: "#DAD7CD",
+        secondary: "#588157",
+      },
     },
-    plugins: [],
+  },
+  plugins: [],
 };
